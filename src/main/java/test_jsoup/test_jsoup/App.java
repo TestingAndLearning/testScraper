dfe9c0b4dae486569568519d0425cece1e935665
MT9304 <MT9304@gmail.com>
2018-07-22 16:19:35 -0700

    Working on ndc.

--- src/main/java/test_jsoup/test_jsoup/App.java
+++ src/main/java/test_jsoup/test_jsoup/App.java
@@ -10,12 +10,16 @@ public class App {
 	
     public static void main( String[] args ) throws IOException, InterruptedException
     {
-        ND1Revenue ND1Revenue = new ND1Revenue("MSFT");
-        System.out.println(ND1Revenue.getRevenueByYears());
-        System.out.println(ND1Revenue.getRevenueByQuarters());
+        //ND1Revenue ND1Revenue = new ND1Revenue("MSFT");
+        //System.out.println(ND1Revenue.getRevenueByYears());
+        //System.out.println(ND1Revenue.getRevenueByQuarters());
         
-        ND1Revenue ND1Revenue2 = new ND1Revenue("DBX");
-        System.out.println(ND1Revenue2.getRevenueByYears());
-        System.out.println(ND1Revenue2.getRevenueByQuarters());
+        //ND1Revenue ND1Revenue2 = new ND1Revenue("DBX");
+        //System.out.println(ND1Revenue2.getRevenueByYears());
+        //System.out.println(ND1Revenue2.getRevenueByQuarters());
+								
+								ND0Calculator ndCalc = new ND0Calculator("MSFT");
+								ndCalc.hasIncreasingAnnualRevenue();
+								ndCalc.hasIncreasingQuarterlyRevenue();
     }
 }
--- src/main/java/test_jsoup/test_jsoup/ND0Calculator.java
+++ src/main/java/test_jsoup/test_jsoup/ND0Calculator.java
@@ -1,6 +1,35 @@
 package test_jsoup.test_jsoup;
 
-public class ND0Calculator
-{
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 
+public class ND0Calculator {
+				String tickerSymbol;
+				ND1Revenue webScraper = new ND1Revenue(tickerSymbol);
+				
+				public ND0Calculator(String tickerSymbol) throws IOException, InterruptedException{
+								this.tickerSymbol = tickerSymbol;
+				}
+				
+				//Past 3 years
+				public boolean hasIncreasingAnnualRevenue() throws IOException, InterruptedException {
+								Map<String, String> revenueByYears = webScraper.getRevenueByYears();
+								Set<String> keys = revenueByYears.keySet();
+								ArrayList<String> allYears = new ArrayList<>();
+								System.out.println(keys.size());
+								for (String k : keys) {
+
+								}
+								return false;
+				}
+				
+				//Past 3 quarters
+				public boolean hasIncreasingQuarterlyRevenue() throws IOException, InterruptedException{
+								Map<String, String> revenueByQuarters = webScraper.getRevenueByQuarters();
+								Set<String> keys = revenueByQuarters.keySet();
+								System.out.println("");
+								return false;
+				}
 }
